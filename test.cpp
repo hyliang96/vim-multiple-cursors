daf6b22e1e481714b2d88c8721469a290e10c3af
Haoyu Liang <hyliang96@163.com>
2019-02-11 16:07:33 +0800

    before try 'not explicity mapping keys'

--- test.cpp
+++ test.cpp
@@ -28,3 +28,45 @@ a
 e
 a
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+asdasd asdasd mulaqwewqe asd as d  asdf
+asdasd asdasd isaqwewqe asd as d  dsa
+asdasd asdasd isaqwewqe asd as d  f
+asdasd asdasd isaqwewqe asd as d  sda
+asdasd asdasd isaqwewqe asd as d  f
+asdasd asas asd as d  as
+asdasd asas asd as d  df
+asdasd asas asd as d  sad
+asdasd asas asd as d  f
+asdasd asas asd as d  sd
+asdasd isaqwewqe asd as d
+isaqwewqe
+isaqwewqe
+isaqwewqe
+isaqwewqe
+isaqwewqe
+isaqwewqe
+isaqwewqe
+isaqwewqe
+isaqwewqe
+isaqwewq
+a
+e
+a
+
